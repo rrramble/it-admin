80a0d2d20ceb95690dfdde7087c94c30f2d98a5c
rrramble <rustam.arkharov@gmail.com>
2026-06-18 00:16:08 +0500

    fix: change directory pointing to the script folder when ran as administrator

--- block-browsers/_all.bat
+++ block-browsers/_all.bat
@@ -1,2 +1,6 @@
+:: Changes current directory to the folder where this script is executed from.
+:: This ensures that the script correctly located instead of "c:\windows\system32".
+cd /d "%~dp0"
+
 ./atom.bat
 ./yandex.bat

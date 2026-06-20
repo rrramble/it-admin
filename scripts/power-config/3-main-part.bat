656ca4e1716b3b9d4a49d3a2c21dfe826f3f0834
rrramble <rustam.arkharov@gmail.com>
2026-06-21 02:07:26 +0500

    docs: use '@echo' in commnets instead of REM, change secitons' numbers

--- scripts/power-config/3-main-part.bat
+++ scripts/power-config/3-main-part.bat
@@ -1,4 +1,4 @@
-REM ! Replace {SCHEME_GUID} below with the needed GUID taken from `powercfg /list` !
+@echo ! Replace {SCHEME_GUID} below with the needed GUID taken from `powercfg /list` !
 @REM Usual GUIDs (but can be other, user-made):
 @REM - High performance: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 @REM - Balanced: 381b4222-f694-41f0-9685-ff5bb260df2e
@@ -6,59 +6,55 @@ REM ! Replace {SCHEME_GUID} below with the needed GUID taken from `powercfg /lis
 set SCHEME_GUID={SCHEME_GUID}
 
 :: ==============================================================
-echo Verifying Administrator privileges
+@echo Verifying Administrator privileges
 net session >nul 2>&1
 if %errorLevel% neq 0 (
     echo [ERROR] This script must be run as an Administrator!
     exit /b 1
 )
 
-@REM ===========================
-REM 0. Activate the power scheme
+:: ===========================
+@echo 1. Activate the power scheme
 powercfg /setactive %SCHEME_GUID%
 
-@REM ===========================
-REM 1. Timeout of switching off the display - AC and DC (minutes)
+:: ===========================
+@echo 2. Timeout of switching off the display - AC and DC (minutes)
 powercfg /change monitor-timeout-ac 120
 powercfg /change monitor-timeout-dc 30
 
-@REM ===========================
-REM 2. USB selective suspend AC and DC (disabled)
+:: ===========================
+@echo 3. USB selective suspend AC and DC (disabled)
 powercfg /setacvalueindex %SCHEME_GUID% 2a737441-1930-4402-8d77-b2bebba308a3 3b04d4fd-1cc7-4f23-ab1c-d1337819c4bb 0
 powercfg /setdcvalueindex %SCHEME_GUID% 2a737441-1930-4402-8d77-b2bebba308a3 3b04d4fd-1cc7-4f23-ab1c-d1337819c4bb 0
 
-@REM ===========================
-REM 3. Sleep timeout AC and DC (minutes)
+:: ===========================
+@echo 4. Sleep timeout AC and DC (minutes)
 powercfg /change standby-timeout-ac 240
 powercfg /change standby-timeout-dc 60
 
-@REM ===========================
-REM 4. Allow sleep AC and DC (disable)
+:: ===========================
+@echo 5. Allow sleep AC and DC (disable)
 powercfg /setacvalueindex %SCHEME_GUID% 238C9FA8-0AAD-41ED-83F4-97BE242C8F20 29F6C1DB-86DA-48C5-9FDB-F2B67B1F44DA 0
 powercfg /setdcvalueindex %SCHEME_GUID% 238C9FA8-0AAD-41ED-83F4-97BE242C8F20 29F6C1DB-86DA-48C5-9FDB-F2B67B1F44DA 1
 
-@REM ===========================
-REM 5. Hibernate timeout AC (disable, command in minutes)
+:: ===========================
+@echo 6. Hibernate timeout AC and DC (disable, command in minutes)
 powercfg /change hibernate-timeout-ac 0
-powercfg /hibernate off
-
-@REM ===========================
-REM 6. Hibernate timeout DC (disable, command in minutes)
 powercfg /change hibernate-timeout-dc 240
 powercfg /hibernate off
 
-@REM ===========================
-REM 7. CPU minimum and maximum processor state AC (%)
+:: ===========================
+@echo 7. CPU minimum and maximum processor state
+@echo 7.1. AC (%)
 powercfg /setacvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 50
 powercfg /setacvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
 
-@REM ===========================
-REM 8. CPU minimum and maximum processor state DC (%)
+@echo 7.2. DC (%)
 powercfg /setdcvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 20
 powercfg /setdcvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 80
 
-@REM ===========================
-REM Apply changes
+:: ===========================
+@echo 8. Apply changes
 powercfg /S %SCHEME_GUID%
 
 echo Power settings updated

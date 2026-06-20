0817b4856f5741921e0a68e6e2e138cf6a8217ec
rrramble <rustam.arkharov@gmail.com>
2026-06-21 03:02:03 +0500

    docs: unify the comments for 'cd /d "%~dp0"' command

--- scripts/no-auto-run/_all.bat
+++ scripts/no-auto-run/_all.bat
@@ -1,5 +1,5 @@
-:: Changes current directory to the folder where this script is executed from.
-:: This ensures that the script correctly located instead of "c:\windows\system32".
+:: Sets the current directory to the folder where this script is executed from,
+:: instead of "c:\windows\system32"
 cd /d "%~dp0"
 
 no-auto-run.bat

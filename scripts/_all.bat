75ca1ec2bcf17af2c6cbdd03aef1f8f65d62eeb8
rrramble <rustam.arkharov@gmail.com>
2026-06-21 03:06:05 +0500

    fix: add 'setlocal enabledelayedexpansion' just in case in the main _all.bat script

--- scripts/_all.bat
+++ scripts/_all.bat
@@ -1,9 +1,14 @@
+:: ==============================================================
 :: Combines and run all nested scripts
+:: ==============================================================
+
+setlocal enabledelayedexpansion
 
 :: Sets the current directory to the folder where this script is executed from,
 :: instead of "c:\windows\system32"
 cd /d "%~dp0"
 call block-browsers/_all.bat
+
 cd /d "%~dp0"
 call no-auto-run/_all.bat
 

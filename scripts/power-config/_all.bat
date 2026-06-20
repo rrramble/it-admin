7898320bf3b335f1c7efc0e764d2558db1f34417
rrramble <rustam.arkharov@gmail.com>
2026-06-21 03:09:42 +0500

    refactor: rename files in power-config

--- scripts/power-config/_all.bat
+++ scripts/power-config/_all.bat
@@ -1,9 +1,7 @@
 :: Sets the current directory to the folder where this script is executed from,
 :: instead of "c:\windows\system32"
 cd /d "%~dp0"
-call 1-screensaver.bat
+call screensaver.bat
 
 cd /d "%~dp0"
-call 3-main-part.bat
-
-:: TODO: rename '3-main-part'
+call power.bat

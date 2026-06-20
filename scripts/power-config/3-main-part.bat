e5a06deb4c4d858c982ed6f4f44c2a15a2b89974
rrramble <rustam.arkharov@gmail.com>
2026-06-21 02:25:06 +0500

    docs: tune comments

--- scripts/power-config/3-main-part.bat
+++ scripts/power-config/3-main-part.bat
@@ -1,4 +1,5 @@
 @echo ! Replace {SCHEME_GUID} below with the needed GUID taken from `powercfg /list` !
+
 @REM Usual GUIDs (but can be other, user-made):
 @REM - High performance: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 @REM - Balanced: 381b4222-f694-41f0-9685-ff5bb260df2e
@@ -18,7 +19,7 @@ if %errorLevel% neq 0 (
 powercfg /setactive %SCHEME_GUID%
 
 :: ===========================
-@echo 2. Timeout of switching off the display - AC and DC (minutes)
+@echo 2. Timeout of switching off the display - AC and DC (in minutes)
 powercfg /change monitor-timeout-ac 120
 powercfg /change monitor-timeout-dc 30
 
@@ -38,17 +39,17 @@ powercfg /setdcvalueindex %SCHEME_GUID% 238C9FA8-0AAD-41ED-83F4-97BE242C8F20 29F
 powercfg /change standby-timeout-dc 30
 
 :: ===========================
-@echo 6. Hibernate timeout AC and DC (disable, command in minutes)
+@echo 5. Hibernate timeout AC and DC (disable, command in minutes)
 powercfg /change hibernate-timeout-ac 0
 powercfg /change hibernate-timeout-dc 240
 powercfg /hibernate off
 
 :: ===========================
-@echo 7. CPU minimum and maximum processor state
-@echo 7.1. AC (%)
+@echo 6. CPU minimum and maximum processor state
+@echo 6.1. AC (%)
 powercfg /setacvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 50
 powercfg /setacvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 100
 
-@echo 7.2. DC (%)
+@echo 6.2. DC (%)
 powercfg /setdcvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 893dee8e-2bef-41e0-89c6-b55d0929964c 20
 powercfg /setdcvalueindex %SCHEME_GUID% 54533251-82be-4824-96c1-47b60b740d00 bc5038f7-23e0-4960-96da-33abaf5935ec 80

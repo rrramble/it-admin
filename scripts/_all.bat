150752131040a16c8bdaee6de5eb271444c95150
rrramble <rustam.arkharov@gmail.com>
2026-06-21 19:32:04 +0500

    refactor: move 'block-browsers' scripts to _manual subfolder

--- scripts/_all.bat
+++ scripts/_all.bat
@@ -7,9 +7,6 @@ setlocal enabledelayedexpansion
 :: Sets the current directory to the folder where this script is executed from,
 :: instead of "c:\windows\system32"
 cd /d "%~dp0"
-call block-browsers/_all.bat
-
-cd /d "%~dp0"
 call internet-noise/_all.bat
 
 cd /d "%~dp0"

2df547d39a31238a91cb94e1bee2d6dae5256c49
rrramble <rustam.arkharov@gmail.com>
2026-06-27 21:59:53 +0500

    refactor: use `reg` instead of `%SystemRoot%\System32\reg.exe`

--- scripts/internet/overall-noise.cmd
+++ scripts/internet/overall-noise.cmd
@@ -83,12 +83,12 @@ set "DEFAULT_HIVE=%SystemDrive%\Users\Default\NTUSER.DAT"
 
 if exist "%DEFAULT_HIVE%" (
     :: Force an unload first to clear any stale or orphaned mounts from previous failures
-    "%SystemRoot%\System32\reg.exe" unload "HKLM\TEMP_DEFAULT" >nul 2>&1
+    reg unload "HKLM\TEMP_DEFAULT" >nul 2>&1
 
     :: Explicitly flush errorlevel back to 0 to wipe out the proactive unload failure code
     cmd /c "exit /b 0"
 
-    "%SystemRoot%\System32\reg.exe" load "HKLM\TEMP_DEFAULT" "%DEFAULT_HIVE%" >nul 2>&1
+    reg load "HKLM\TEMP_DEFAULT" "%DEFAULT_HIVE%" >nul 2>&1
     if !errorLevel! equ 0 (
         :: Wrapped inside a conditional block ensuring execution occurs only when loaded successfully
         set   "KEY_DEF_PRIV=HKLM\TEMP_DEFAULT\Software\Microsoft\Windows\CurrentVersion\Privacy"
@@ -96,23 +96,23 @@ if exist "%DEFAULT_HIVE%" (
         set    "KEY_DEF_CDM=HKLM\TEMP_DEFAULT\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
         set "KEY_DEF_POLICY=HKLM\TEMP_DEFAULT\Software\Policies\Microsoft\Windows"
 
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_PRIV!" /v TailoredExperiencesWithDiagnosticDataEnabled /t REG_DWORD /d 0 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_ADV!" /v Enabled /t REG_DWORD /d 0 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_CDM!" /v ContentDeliveryAllowed /t REG_DWORD /d 0 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_CDM!" /v SoftLandingEnabled /t REG_DWORD /d 0 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_POLICY!\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_POLICY!\Windows Error Reporting" /v DoNotSendAdditionalData /t REG_DWORD /d 1 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_POLICY!\Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_PRIV!" /v TailoredExperiencesWithDiagnosticDataEnabled /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_ADV!" /v Enabled /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_CDM!" /v ContentDeliveryAllowed /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_CDM!" /v SoftLandingEnabled /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_POLICY!\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f >nul
+        reg add "!KEY_DEF_POLICY!\Windows Error Reporting" /v DoNotSendAdditionalData /t REG_DWORD /d 1 /f >nul
+        reg add "!KEY_DEF_POLICY!\Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f >nul
 
         set "KEY_DEF_INPUT=HKLM\TEMP_DEFAULT\Software\Microsoft\InputPersonalization"
         set "KEY_DEF_INPUT_TR=HKLM\TEMP_DEFAULT\Software\Microsoft\InputPersonalization\TrainedDataStore"
 
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_INPUT!" /v RestrictImplicitTextCollection /t REG_DWORD /d 1 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_INPUT!" /v RestrictImplicitInkCollection /t REG_DWORD /d 1 /f >nul
-        "%SystemRoot%\System32\reg.exe" add "!KEY_DEF_INPUT_TR!" /v HarvestedWords /t REG_DWORD /d 0 /f >nul
+        reg add "!KEY_DEF_INPUT!" /v RestrictImplicitTextCollection /t REG_DWORD /d 1 /f >nul
+        reg add "!KEY_DEF_INPUT!" /v RestrictImplicitInkCollection /t REG_DWORD /d 1 /f >nul
+        reg add "!KEY_DEF_INPUT_TR!" /v HarvestedWords /t REG_DWORD /d 0 /f >nul
 
         :: Mandatory clean unload
-        "%SystemRoot%\System32\reg.exe" unload "HKLM\TEMP_DEFAULT" >nul
+        reg unload "HKLM\TEMP_DEFAULT" >nul
         echo [OK] Default profile updated successfully.
     ) else (
         echo [WARN] Failed to load Default Profile hive. Code: !errorLevel!

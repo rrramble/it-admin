d92fa3f5af8fb2e83fa624f578b7b92d43a8b46c
rrramble <rustam.arkharov@gmail.com>
2026-06-27 08:48:02 +0500

    refactor: move 'no-auto-run' to 'scripts/secrurity/' folder from 'scripts/no-auto-run/'

--- scripts/no-auto-run/_all.bat
+++
@@ -1,4 +0,0 @@
-:: Sets the current directory to the folder where this script is executed from,
-:: instead of "c:\windows\system32"
-cd /d "%~dp0"
-call no-auto-run.bat
--- scripts/security/_all.bat
+++ scripts/security/_all.bat
@@ -1,6 +1,9 @@
 :: Sets the current directory to the folder where this script is executed from,
 :: instead of "c:\windows\system32"
 cd /d "%~dp0"
+call no-auto-run.cmd
+
+cd /d "%~dp0"
 call security.bat
 
 cd /d "%~dp0"

d421a904f0f80d5e7f47ad654ac754a96ac040db
rrramble <rustam.arkharov@gmail.com>
2026-07-25 01:52:35 +0500

    feat: add 'remote outlook setup' script

---
+++ outlook/remote-setup/_run.cmd
@@ -0,0 +1,20 @@
+@echo off
+:: ===============================================================================
+:: REMOTE OUTLOOK CONFIGURATION
+:: This script creates Outlook profile using `PsExec` program
+::
+:: Requirements:
+:: - `Psexec.exe` should be available.
+:: - Run this code as administrator.
+:: - Remote user must be logged in.
+:: - Outlook should be installed on the remote computer.
+
+set "TARGET_PC=PC-01"
+set "SHARED_PRF_PATH=config.prf"
+
+:: Note: Update the path below for your version of Outlook:
+:: For Office 2010: "C:\Program Files\Microsoft Office\Office14\OUTLOOK.EXE"
+:: For Office 2013: "C:\Program Files\Microsoft Office\Office15\OUTLOOK.EXE"
+:: For Office 2016 / 2019 / 2021 / 365: "C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE"
+
+psexec \\%TARGET_PC% -i -d "C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE" /importprf "%SHARED_PRF_PATH%"

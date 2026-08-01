6804561531ba40e8cc7fe5450b8ed60657c24ec3
rrramble <rustam.arkharov@gmail.com>
2026-08-01 18:24:01 +0500

    docs: tune comments

--- scripts/common/windows-noise.cmd
+++ scripts/common/windows-noise.cmd
@@ -19,18 +19,18 @@ if !errorLevel! neq 0 (
 )
 
 :: ======================
-:: Policy to disable Copilot
+:: Disables Copilot
 :: https://learn.microsoft.com/en-us/windows/client-management/mdm/policy-csp-windowsai#:~:text=TurnOffWindowsCopilot
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Copilot" /v TurnOffWindowsCopilot /t REG_DWORD /d 1 /f
 
 :: ======================
-:: Deletes XPS writer
+:: Deletes "XPS writer" printer
 :: Relies on the exact printer name, will not work if the object is renamed
 printui.exe /dl /n "Microsoft XPS Document Writer" /q
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Printers\Microsoft XPS Document Writer" /f
 
 :: ======================
-:: Deletes Fax
+:: Deletes "Fax" printer
 :: Relies on the exact printer name, will not work if the object is renamed
 printui.exe /dl /n "Fax" /q
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Printers\Fax" /f
